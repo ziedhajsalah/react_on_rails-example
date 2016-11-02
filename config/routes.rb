b22941dc7c90d845f01a00eaa3794679c3a471b3
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2016-11-02 17:00:27 +0100

    add profile managment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'profile' => 'profile#index' # show the profile page
+  patch 'profile' => 'profile#update' # update the profile
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
